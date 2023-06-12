ab0442c5074e996310228585954703aa1609a25c
Isaac Greenwood <igreenwoodwfg@gmail.com>
2023-06-11 22:59:52 -0400

    final streamlined submission of team-4-m-1-capstone

--- src/main/java/com/techelevator/filereader/LogFileWriter.java
+++ src/main/java/com/techelevator/filereader/LogFileWriter.java
@@ -11,17 +11,31 @@ import java.time.format.DateTimeFormatter;
  */
 public class LogFileWriter {
 
+
+    // Declare a constant to call the file pathway for the log file
     private static String LOG_FILE_PATHWAY = "log.txt";
+
+
+    // Declaration of variables using LocalDateTime and DateTimeFormatter Libraries
+    private LocalDateTime timeNow = LocalDateTime.now();
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss a");
+
+    // timeStamp instance variable declaration
+    private String timeStamp;
+
+    public String getTimeStamp() {
+        return timeStamp = timeNow.format(formatter);
+    }
+
+    // Method Constructor for LogFileWriter Class
     public void writeLoggedActivity(String command, double initialCustomerBalance,
                                     double finalCustomerBalance){
 
-        LocalDateTime timeNow = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss a");
-        String timestamp = timeNow.format(formatter);
-
-        String logEntry = String.format("%s %s $%.2f $%.2f", timestamp, command,
+        // Declaration to create a string containing information to be written as a line in the log.txt file
+        String logEntry = String.format("%s %s $%.2f $%.2f", getTimeStamp(), command,
                 initialCustomerBalance, finalCustomerBalance);
 
+        // try/catch to access the log.txt file and append it to the existing file
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOG_FILE_PATHWAY, true))) {
             writer.write(logEntry);
             writer.newLine();
